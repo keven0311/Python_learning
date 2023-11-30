a2771c3584fbb17b50956eaff070105897e91f22
kaichong lin <coco.keven@gmail.com>
2023-11-30 00:08:13 -0600

    chapter int

---
+++ calculator.py
@@ -0,0 +1,16 @@
+x = int(input("what's int x? "))
+y = float(input("what's float y? "))
+
+# round the number to intiger:
+z = round(x+y)
+
+# use f string with ":," for a number, it will seperate the number with ","
+
+print(f'the answer is rounded: {z:,}')
+
+# use ":.2f" will rounded the float number with two digits after "."
+
+print(f"{z:.2f}")
+
+# it also works fine using round method like this:
+zz = round(x / y, 2)
--- hello.py
+++ hello.py
@@ -25,3 +25,4 @@ first, last = name.split(" ")
 print(f"Hello, {first}!")
 
 # testing for github pushes!
+
