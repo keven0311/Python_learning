66f98d2e16509ccb9a438186adda71c6bd0e655a
kaichong lin <coco.keven@gmail.com>
2023-12-08 01:58:10 -0600

    modules and packages

--- name.py
+++ name.py
@@ -5,12 +5,12 @@ import sys
 # the "name.py" is sys.argv[0] and "Kev" is the sys.argv[1]
 # if you dont give the sys.argv[1] in this situation, it will casue an IndexError.
 
-if len(sys.argv) < 2:
-    sys.exit('too few arguments')
-elif len(sys.argv) > 2:
-    sys.exit('too many arguments')
-else:
-    print('hello, my name is', sys.argv[1])
+# if len(sys.argv) < 2:
+#     sys.exit('too few arguments')
+# elif len(sys.argv) > 2:
+#     sys.exit('too many arguments')
+# else:
+#     print('hello, my name is', sys.argv[1])
     
 #-----------------------------
 if len(sys.argv) < 2:
