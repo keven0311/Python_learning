c42adcd841f0702d638498404f377fc437ab84cd
kaichong lin <coco.keven@gmail.com>
2023-12-07 23:45:30 -0600

    modules

---
+++ generate.py
@@ -0,0 +1,39 @@
+# import random
+
+# coin = random.choice(['heads', 'tails'])
+
+# print(coin)
+
+#-------------------------------
+
+# from random import choice
+
+# coin = choice(['heads', 'tails'])
+
+# print(coin)
+
+#--------------------------------
+
+# import random
+
+# # number = random.randint(1,10)
+
+# # print(number)
+
+# cards = ['jack', 'queen' , 'king']
+
+# # shuffle will make the list in a random order:
+# random.shuffle(cards)
+
+# for card in cards:
+#     print(card)
+
+#---------------------------------
+
+
+import statistics
+
+# statistics.mean will give you the mean of two integers:
+print(statistics.mean([100,90]))
+
+
---
+++ name.py
@@ -0,0 +1,26 @@
+import sys
+
+
+# sys.argv[1] means in command line if you run: "python3 name.py Kev". 
+# the "name.py" is sys.argv[0] and "Kev" is the sys.argv[1]
+# if you dont give the sys.argv[1] in this situation, it will casue an IndexError.
+
+if len(sys.argv) < 2:
+    sys.exit('too few arguments')
+elif len(sys.argv) > 2:
+    sys.exit('too many arguments')
+else:
+    print('hello, my name is', sys.argv[1])
+    
+#-----------------------------
+if len(sys.argv) < 2:
+    sys.exit("too few arguments")
+
+for arg in sys.argv[1:-1]:
+    print('hello, my name is', arg)
+    
+#-----------------------------
+# try:
+#     print('hello, my name is', sys.argv[1])
+# except IndexError:
+#     print('Too few arguments')
