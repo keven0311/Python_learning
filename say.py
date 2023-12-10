e1797f4b985d4e04eb692776918eddfb42c2a8ef
kaichong lin <coco.keven@gmail.com>
2023-12-09 18:13:37 -0600

    module imports

--- say.py
+++ say.py
@@ -1,5 +1,10 @@
 import cowsay
 import sys
 
+# if len(sys.argv) == 2:
+#     cowsay.trex('hello, ' + sys.argv[1])
+    
+from saying import hello
+
 if len(sys.argv) == 2:
-    cowsay.trex('hello, ' + sys.argv[1])
+    hello(sys.argv[1])
